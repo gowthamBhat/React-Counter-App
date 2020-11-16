0bfa5e390e63ccb86ce954a23b897b7f24e7bd6d
Gowtham  H D <gowthambhat793@gmail.com>
2020-11-16 13:18:25 +0530

    incrementing counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,6 +17,9 @@ class Counter extends Component {
         classes += count === 0 ? "warning" : "primary";
         return classes;                          // zero count idre yellow color barathe illa blue
     }
+    incrementHandler = () => {
+        this.setState({ count: this.state.count + 1 }) //increment the count value, also can pass a arrow function in setState
+    }
     render() {
         const { count } = this.state;
         let classes = this.badgeColorFormatter(count);  // zero count idre yellow color barathe illa blue
@@ -26,10 +29,10 @@ class Counter extends Component {
                 <h3 style={{ color: "blue" }}>using css inline</h3> */}
 
                 <span className={classes}>{this.formatter()}</span>
-                <button className="btn btn-secondary btn-sm">increment</button>
+                <button className="btn btn-secondary btn-sm" onClick={this.incrementHandler}>increment</button>
             </>
         );
-    }
+    } //onClick alli this.increment call mado badlu arrow function call madbodu, argumnet pass madod idre help agathe
 
 
 }
